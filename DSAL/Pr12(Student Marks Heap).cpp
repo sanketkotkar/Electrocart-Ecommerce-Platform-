ef7f62f6ebef890988608be7e448adac59bf1919
Sanket Mohan Kotkar <sanketkotkar016@gmail.com>
2025-05-05 17:44:16 +0530

    Update Pr12(Student Marks Heap).cpp

--- DSAL/Pr12(Student Marks Heap).cpp
+++ DSAL/Pr12(Student Marks Heap).cpp
@@ -4,11 +4,16 @@ using namespace std;
 class heap1 {
 public:
     void MAX_HEAPIFY(int a[], int i, int n) {
-        int l = 2 * i, r = (2 * i + 1), largest, loc;
-        largest = (l <= n && a[l] > a[i]) ? l : i;
-        if (r <= n && a[r] > a[largest]) largest = r;
+        int l = 2 * i;
+        int r = 2 * i + 1;
+        int largest = (l <= n && a[l] > a[i]) ? l : i;
+
+        if (r <= n && a[r] > a[largest]) {
+            largest = r;
+        }
+
         if (largest != i) {
-            loc = a[i];
+            int loc = a[i];
             a[i] = a[largest];
             a[largest] = loc;
             MAX_HEAPIFY(a, largest, n);
@@ -16,7 +21,9 @@ public:
     }
 
     void BUILD_MAX_HEAP(int a[], int n) {
-        for (int k = n / 2; k >= 1; k--) MAX_HEAPIFY(a, k, n);
+        for (int k = n / 2; k >= 1; k--) {
+            MAX_HEAPIFY(a, k, n);
+        }
     }
 
     void HEAPSORT(int a[], int n) {
@@ -31,18 +38,26 @@ public:
         int n;
         cout << "Enter the number of students" << endl;
         cin >> n;
-        int a[n];
+
+        int a[n + 1];  // +1 to safely use index from 1 to n
+
         cout << "Enter the marks of the students " << endl;
-        for (int i = 1; i <= n; i++) cin >> a[i];
+        for (int i = 1; i <= n; i++) {
+            cin >> a[i];
+        }
+
         HEAPSORT(a, n);
         display(a, n);
     }
 
     void display(int a[], int n) {
         cout << ":::::::SORTED MARKS::::::" << endl;
-        for (int i = 1; i <= n; i++) cout << a[i] << endl;
-        cout << "Minimum marks obtained are:" << a[1];
-        cout << "\nMaximum marks obtained are:" << a[n];
+        for (int i = 1; i <= n; i++) {
+            cout << a[i] << endl;
+        }
+
+        cout << "Minimum marks obtained are: " << a[1] << endl;
+        cout << "Maximum marks obtained are: " << a[n] << endl;
     }
 };
 
